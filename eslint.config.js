177874a755026fff0dd80672f3970ebfe07f29ad
thnielseen <tess.prog@gmail.com>
2025-01-23 19:02:29 +0100

    🔧🙈 Update config and .gitignore files

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      camelcase: 'error',
+      'comma-dangle': ['error', 'always-multiline'], // Enforce trailing commas in multiline
+      curly: ['error', 'all'], // Require curly braces for all control statements
+      'no-console': 'off', // Warn about console.log usage
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];
